e1be184593ce679e58c57fa600404524fc3c9f20
s321h321 <317162944@qq.com>
2015-06-11 16:01:44 +0800

    更新jetty配置细腻

--- mvcspring/src/main/java/com/lolaage/controller/LoginController.java
+++ mvcspring/src/main/java/com/lolaage/controller/LoginController.java
@@ -5,30 +5,36 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.ModelAndView;
 
 import com.lolaage.service.ILoginService;
+
 @RequestMapping("/loginController")
 @Controller
-public class LoginController {
-
-	@Resource
-	private ILoginService loginService;
-	@RequestMapping("/login")
-	public String Login(String username,String password,ModelMap map){
-		if(loginService.Login(username,password)){
-			System.out.println("success.."+username+"-password:"+password);
-			map.put("u", username);
-			map.put("p", password);
-			return "/pages/main";	
-		}
-		else{
-			map.put("msg", "帐号或密码错误!!!");
-			System.out.println("back login");
-			return "/pages/Login/login";
-		}
-//		
-
-	}
-	
+public class LoginController
+{
+    
+    @Resource
+    private ILoginService loginService;
+    
+    @RequestMapping("/login")
+    public String Login(String username, String password, ModelMap map)
+    {
+        
+        if (loginService.Login(username, password))
+        {
+            System.out.println("success.." + username + "-password:" + password);
+            map.put("u", username);
+            map.put("p", password);
+            return "/pages/main";
+        }
+        else
+        {
+            map.put("msg", "帐号或密码错误!!!");
+            System.out.println("back login");
+            return "/pages/Login/login";
+        }
+        //
+        
+    }
+    
 }
